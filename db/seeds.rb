5a9757838a4c133082169680e90b77ae60a46796
Freddy Kurniawan <freddykwan.work@gmail.com>
2018-02-21 09:11:14 +0800

    mister fucking cocktail milestone

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,6 @@ cocktails = [
     photo: "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
     },
   {
-    name: "Daiquiri",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
-    },
-  {
     name: "Margarita",
     photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
     },
@@ -26,82 +22,22 @@ cocktails = [
     photo: "http://www.seriouseats.com/images/2014/11/20141101-cognac-sidecar-carey-jones.jpg"
     },
   {
-    name: "French 75",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-french75.jpg"
-    },
-  {
     name: "Bloody Mary",
     photo: "http://www.seriouseats.com/images/2015/03/twase-20150320-21.jpg"
-    },
-  {
-    name: "Irish Coffee",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-irish-coffee.jpg"
-    },
-  {
-    name: "Jack Rose",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-jack-rose.jpg"
-    },
-  {
-    name: "Negroni",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-negroni.jpg"
-    },
-  {
-    name: "Boulevardier",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-boulevardier.jpg"
-    },
-  {
-    name: "Sazerac",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-sazerac.jpg"
-    },
-  {
-    name: "Vieux Carré",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-vieux-carre.jpg"
-    },
-  {
-    name: "Ramos Gin Fizz",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-robyn-lee-ramos-gin-fizz.jpg"
-    },
-  {
-    name: "Mint Julep",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mint-julep.jpg"
-    },
-  {
-    name: "Whiskey Sour",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-whiskey-sour.jpg"
     },
   {
     name: "Mai Tai",
     photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mai-tai.jpg"
     },
-  {
-    name: "Planter's Punch",
-    photo: "http://www.seriouseats.com/images/2015/04/20150406-cocktails-planters-punch-robyn-lee-1.jpg"
-    },
-  {
-    name: "Pisco Sour",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-pisco-sour.jpg"
-    },
-  {
-    name: "Cosmopolitan",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-cosmopolitan.jpg"
-    },
-  {
-    name: "Tom Collins",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-tom-collins.jpg"
-    },
-  {
-    name: "Last Word",
-    photo: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-last-word.jpg"
-  }
 ]
 
 ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
 ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
-cocktails.each do |cocktail|
-  new_cocktail = Cocktail.new(cocktail)
-  new_cocktail.remote_photo_url = cocktail[:photo]
+cocktails.each do |data|
+  new_cocktail = Cocktail.new(name: data[:name])
+  new_cocktail.remote_photo_url = data[:photo]
   new_cocktail.save
 end
 
